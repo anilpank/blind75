167e476a86454568d3b490d5cc2b8c7bc83509c9
Anil <anil.iitk@gmail.com>
2024-02-03 11:39:38 -0800

    Create BestTimeBuySellStock.java

---
+++ BestTimeBuySellStock.java
@@ -0,0 +1,60 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+class BestTimeBuySellStock {
+    public int maxProfitBruteForce(int[] prices) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int maxProfit = 0;
+        for (int i=0; i<prices.length; i++) {
+            int c = prices[i];
+            for (int j=i+1; j<prices.length; j++) {
+                int d = prices[j];
+                if (maxProfit<d-c) {
+                    maxProfit = d-c;
+                }
+            }
+
+        }
+        return maxProfit;
+    }
+
+    public int maxProfit(int[] prices) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int  p : prices) {
+            min = Math.min(min, p);
+            max = Math.max(max, p-min);
+        }
+        return max;
+    }
+
+
+    public int maxProfitOptimum(int[] prices) {
+        int maxProfit = 0, left = 0, right = 1;
+        if (prices.length == 0) return 0;
+
+        while (right<prices.length) {
+            int diff = prices[right] - prices[left];
+            if (diff<0) {
+                left = right;
+            }
+            else   {
+                 maxProfit = Math.max(diff,maxProfit);
+                
+            }
+            right++;
+            
+        } 
+        return maxProfit;
+    }
+}
+
+/**
+
+input
+- arr of prices, stock price on ith day
+
+condition, output
+- max profit 
+
+
+ */
